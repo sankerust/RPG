38a58098d28f12a2012f37236bf6197854592056
sanke <egor.tyukin@gmail.com>
2020-11-05 17:39:54 +0300

    import provided assets add player character make navmesh adjust navmesh add animations to character make point and click movement

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,19 +1,44 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class Mover : MonoBehaviour
 {
-  [SerializeField] Transform target;
-    // Start is called before the first frame update
+  NavMeshAgent navMeshAgent;
+  Animator animator;
+
     void Start()
     {
+      navMeshAgent = GetComponent<NavMeshAgent>();
+      animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        GetComponent<NavMeshAgent>().destination = target.position;
+      if (Input.GetMouseButtonDown(0)) {
+        MoveToCursor();
+      }
+
+      UpdateAnimator();
     }
+
+  private void UpdateAnimator()
+  {
+    Vector3 velocity = navMeshAgent.velocity;
+    Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+    float speed = localVelocity.z;
+    animator.SetFloat("forwardSpeed", speed);
+  }
+
+  private void MoveToCursor()
+  {
+    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    RaycastHit hit;
+    bool hasHit = Physics.Raycast(ray, out hit);
+    if (hasHit) {
+      navMeshAgent.destination = hit.point;
+    }
+  }
 }
