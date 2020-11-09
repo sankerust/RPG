d2dc7889c6563480fd76f47b2ef619d68746594e
sanke <egor.tyukin@gmail.com>
2020-11-09 18:43:53 +0300

    add attack behaviour

--- Assets/Scripts/Core/FollowCamera.cs
+++ Assets/Scripts/Core/FollowCamera.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FollowCamera : MonoBehaviour
+namespace RPG.Core
 {
-  [SerializeField] Transform target;
+  public class FollowCamera : MonoBehaviour
+  {
+    [SerializeField] Transform target;
 
     void LateUpdate()
     {
-        transform.position = Vector3.Lerp(transform.position, target.position, 0.05f);
+      transform.position = Vector3.Lerp(transform.position, target.position, 0.05f);
     }
+  }
 }
--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -4,41 +4,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Mover : MonoBehaviour
+namespace RPG.Movement
 {
-  NavMeshAgent navMeshAgent;
-  Animator animator;
+  public class Mover : MonoBehaviour
+  {
+    
+    [SerializeField] Transform target;
+    Animator animator;
+    NavMeshAgent navMeshAgent;
+
 
     void Start()
     {
-      navMeshAgent = GetComponent<NavMeshAgent>();
       animator = GetComponent<Animator>();
+      navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
-    void Update()
+    private void Update()
     {
-      if (Input.GetMouseButton(0)) {
-        MoveToCursor();
-      }
-
       UpdateAnimator();
     }
 
-  private void UpdateAnimator()
-  {
-    Vector3 velocity = navMeshAgent.velocity;
-    Vector3 localVelocity = transform.InverseTransformDirection(velocity);
-    float speed = localVelocity.z;
-    animator.SetFloat("forwardSpeed", speed);
-  }
+    public void MoveTo(Vector3 destination)
+    {
+        navMeshAgent.destination = destination;
+        navMeshAgent.isStopped = false;
+    }
 
-  private void MoveToCursor()
-  {
-    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-    RaycastHit hit;
-    bool hasHit = Physics.Raycast(ray, out hit);
-    if (hasHit) {
-      navMeshAgent.destination = hit.point;
+    public void Stop() {
+      navMeshAgent.isStopped = true;
+    }
+
+    private void UpdateAnimator()
+    {
+      Vector3 velocity = navMeshAgent.velocity;
+      Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+      float speed = localVelocity.z;
+      animator.SetFloat("forwardSpeed", speed);
     }
   }
 }
