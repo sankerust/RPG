7bac6f256c203caa5cfc20971dbff7c238d79fde
sanke <egor.tyukin@gmail.com>
2020-12-05 16:08:01 +0300

    add second placeholder scene add portal to load scene

--- Assets/Scripts/Cinematics/CinematicControlremover.cs
+++ Assets/Scripts/Cinematics/CinematicControlremover.cs
@@ -1,3 +1,5 @@
+using RPG.Control;
+using RPG.Core;
 using UnityEngine;
 using UnityEngine.Playables;
 
@@ -7,19 +9,22 @@ namespace RPG.Cinematics
   
   public class CinematicControlremover : MonoBehaviour {
     
+    GameObject player;
 
     private void Start() {
       GetComponent<PlayableDirector>().played += DisableControl;
       GetComponent<PlayableDirector>().stopped += EnableControl;
+      player = GameObject.FindWithTag("Player");
     }
 
 
     void DisableControl(PlayableDirector pd) {
-      print("DisableControl");
-    }
+      player.GetComponent<ActionScheduler>().CancelCurrentAction();
+      player.GetComponent<PlayerController>().enabled = false;
+      }
 
     void EnableControl(PlayableDirector pd) {
-      print("EnableControl");
+      player.GetComponent<PlayerController>().enabled = true;
     }
   }
 }
---
+++ Assets/Scripts/Core/SceneManagement/Portal.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RPG.SceneManagement
+{
+public class Portal : MonoBehaviour
+  {
+    [SerializeField] int portalSceneIndex;
+    private void OnTriggerEnter(Collider other) {
+      if (other.gameObject.tag == "Player") {
+        SceneManager.LoadScene(portalSceneIndex);
+      }
+    }
+  }
+}
+
