ae780d14d92265229152334015b03ea765de8c6f
sankerust <egor.tyukin@gmail.com>
2020-12-15 18:32:49 +0300

    add fader

---
+++ Assets/Scripts/Core/SceneManagement/Fader.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace RPG.SceneManagement {
+    public class Fader : MonoBehaviour
+    {
+        CanvasGroup canvasGroup;
+        private void Start() {
+            canvasGroup = GetComponent<CanvasGroup>();
+
+            StartCoroutine(FadeOutIn());
+        }
+
+        IEnumerator FadeOutIn() {
+            yield return FadeOut(3f);
+            print("Faded out");
+            yield return FadeIn(2f);
+            print("faded in");
+        }
+        public IEnumerator FadeOut(float time) {
+            while (canvasGroup.alpha < 1) {
+                canvasGroup.alpha += Time.deltaTime / time;
+                yield return null;
+            }
+        }
+
+        public IEnumerator FadeIn(float time) {
+            while (canvasGroup.alpha > 0) {
+                canvasGroup.alpha -= Time.deltaTime / time;
+                yield return null;
+            }
+        }
+    }
+}
